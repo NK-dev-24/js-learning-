209c099b1d84983a0771f39e42d626eae6bb34f3
NK-dev-24 <111829908+NK-dev-24@users.noreply.github.com>
2024-10-26 22:32:51 +0530

    Learn Display & Variable

---
+++ main.js
@@ -0,0 +1,11 @@
+// Display
+// alert('Hello World');
+// console.log("hello")
+// console.error("You got a error")
+// console.warn("It a warning")
+
+// Variable - var, let, const
+
+// const age=10;
+// age = 20;
+// console.log(age)
