0ffaf8030282d41affc4adc913146377c6fde7f3
NK-dev-24 <111829908+NK-dev-24@users.noreply.github.com>
2024-10-26 23:00:08 +0530

    Learn Datatype & Concat

--- main.js
+++ main.js
@@ -5,7 +5,26 @@
 // console.warn("It a warning")
 
 // Variable - var, let, const
-
 // const age=10;
 // age = 20;
 // console.log(age)
+
+// Datatype - string, number, Boolean, null, undefined
+// const name = 'Naveen';
+// const age = 23;
+// const rating = 4.9;
+// const isMass = true;
+// const x= null;
+// const y= undefined;
+// let z;
+// console.log(typeof isMass)
+
+// concatination
+// const name = 'Naveen';
+// const age = 23;
+// console.log("My name is "+ name+" and my age is "+age);
+
+// Another way of concatination
+// const hello = `My name is ${name} and age is ${age}`;
+// console.log(hello);
+
