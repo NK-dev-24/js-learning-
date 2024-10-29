f237eae3e009fe945b182f901605a75577bd3f91
NK-dev-24 <111829908+NK-dev-24@users.noreply.github.com>
2024-10-29 15:06:50 +0530

    1hr module complete

--- JS Crash Course/main.js
+++ JS Crash Course/main.js
@@ -20,8 +20,26 @@
 
 //type conversion - Implisit, Explisit
 //implisit
-console.log(4*'3')
+// console.log(4*'3')
 //explisit
 //parseFloat,parseInt,Number,String ()
-console.log((540).toString( ))
-console.log()
+// console.log((540).toString( ))
+// console.log(Boolean(''))
+
+// Equality - == ===
+// === Compare both value & its type. If both are equal - True
+// == While comparing it uses Dynamic converson. So it may be confusing
+
+//condition statement - If, else, else if, switch
+// switch( color ){
+//   case 'red':
+//     console.log('Color is red')
+//     break
+//   case 'blue':
+//     console.log('Color is Blue')
+//     break
+//   default:
+//     console.log('No matching color')
+// }
+
+//loopling Statement - For, While, do While, for of 
