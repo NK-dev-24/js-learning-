a94b50b30b3a3e5d2a298c84e226a748b5f7473d
NK-dev-24 <111829908+NK-dev-24@users.noreply.github.com>
2024-10-31 11:58:23 +0530

    Add mini project

---
+++ 1. JavaScript Crash Course -  Traversy Media/Mini Project/main.js
@@ -0,0 +1,13 @@
+//selectors - Single, Multiple Element
+//Single elements
+// console.log(document.getElementById('my-form'));
+// console.log(document.querySelector('h1'));
+
+//Multiple elements
+console.log(document.querySelectorAll('.item'));
+
+
+
+//learning:
+// console.log(window);
+// window.alert(1);
