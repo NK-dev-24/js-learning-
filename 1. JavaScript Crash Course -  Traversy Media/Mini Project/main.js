b163597a1025de84f999bcaa22a5ee369491131c
NK-dev-24 <111829908+NK-dev-24@users.noreply.github.com>
2024-10-31 13:03:04 +0530

    Complete Module dom

--- 1. JavaScript Crash Course -  Traversy Media/Mini Project/main.js
+++ 1. JavaScript Crash Course -  Traversy Media/Mini Project/main.js
@@ -1,13 +1,43 @@
-//selectors - Single, Multiple Element
-//Single elements
-// console.log(document.getElementById('my-form'));
-// console.log(document.querySelector('h1'));
 
-//Multiple elements
-console.log(document.querySelectorAll('.item'));
 
 
 
 //learning:
 // console.log(window);
 // window.alert(1);
+
+//selectors - Single, Multiple Element
+//Single elements
+// console.log(document.getElementById('my-form'));
+// console.log(document.querySelector('h1'));
+
+//Multiple elements
+// console.log(document.querySelectorAll('.item')); //perfered
+// console.log(document.getElementsByClassName('item')) 
+// console.log(document.getElementsByTagName('li'))
+// old method not prefered 
+
+//loop
+// const items = document.querySelectorAll('.item');
+// items.forEach((item) => console.log(item))
+
+//DOM
+
+// const ul = document.querySelector('.items');
+// ul.remove();
+// ul.firstElementChild.remove()
+// ul.lastElementChild.textContent = 'Hi';
+// ul.children[1].innerHTML = '<h1> Dom Changed <h1>'
+
+// const btn =  document.querySelector('.btn');
+
+// // btn.style.background = 'red';
+// btn.addEventListener('click', (e) => {
+//   e.preventDefault();
+//   document.querySelector('#my-form').style.background = '#333333'
+//   alert('You submitted')
+// })
+// btn.addEventListener('mouseover', (b) => {
+//   b.preventDefault()
+//   document.querySelector('.btn').style.background='#737'
+// })
