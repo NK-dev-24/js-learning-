a5708acc72281e3aa5e4d9b412dbe02aecf35d2f
NK-dev-24 <111829908+NK-dev-24@users.noreply.github.com>
2024-10-31 13:36:32 +0530

    complete Mini project

--- 1. JavaScript Crash Course -  Traversy Media/Mini Project/main.js
+++ 1. JavaScript Crash Course -  Traversy Media/Mini Project/main.js
@@ -1,5 +1,32 @@
+const myForm = document.querySelector('#my-form');
+const nameInput= document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const msg = document.querySelector('.msg');
+const userList= document.querySelector('#users');
 
+myForm.addEventListener('submit', onSubmit);
 
+function onSubmit(e) {
+  e.preventDefault();
+
+  if(nameInput.value === '' || emailInput.value === '') {
+    //msg style and text
+    msg.classList.add('error');
+    msg.innerHTML = 'Please enter all fields'
+    //remove msg after 3sec
+    setTimeout(() => msg.remove(), 3000)
+
+  } else{
+    //add user to ui
+    const li = document.createElement('li');
+    li.appendChild(document.createTextNode(`${nameInput.value} : ${emailInput.value}`))
+    userList.appendChild(li);
+    
+    //clear value
+    nameInput.value='';
+    emailInput.value='';
+  }
+}
 
 
 //learning:
